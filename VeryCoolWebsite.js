8ade0113383085c19190de954d5fc5b2b42037fb
Abraham  Gonzalez <ag32205@srcs.loc>
2018-05-17 12:19:33 -0700

    sounds

--- VeryCoolWebsite.js
+++ VeryCoolWebsite.js
@@ -1,33 +1,17 @@
+var src = document.createAttribute('src');
+src.value = "Pics/riku.jpg";
 
-function Box(xpos, ypos, xstep, ystep, id) {
-    this.xpos = xpos;
-    this.ypos = ypos;
-    this.xstep = xstep;
-    this.ystep = ystep;
-    this.id = id;
-    var elem = document.getElementById(this.id);
-    this.render = function () {
-        elem.style.top = this.ypos + 'px';
-        elem.style.left = this.xpos + 'px';
-    }
+
+document.getElementById("riku").onmouseover = function() {mouseOver()};
+document.getElementById("riku").onmouseout = function() {mouseOut()};
+
+function mouseOver() {
+battle.play();
 }
 
-var box1 = new Box(0, 0, 1, 2, "riku");
-
-var id = setInterval(frame, 5);
-
-function frame() {
-
-    for (var i = 0; i < boxes.length; i++) {
-        if (boxes[i].xpos > 350 || boxes[i].xpos < 0) {
-            boxes[i].xstep = -boxes[i].xstep;
-        }
-        if (boxes[i].ypos > 350 || boxes[i].ypos < 0) {
-            boxes[i].ystep = -boxes[i].ystep;
-        }
-
-        boxes[i].xpos = boxes[i].xpos + boxes[i].xstep;
-        boxes[i].ypos = boxes[i].ypos + boxes[i].ystep;
-        boxes[i].render();
-    }
+function mouseOut() {
+  battle.pause();
 }
+var battle = new Audio('Sounds/battle.mp3')
+var snd = new Audio('Sounds/give_it_all.mp3');
+snd.play();
